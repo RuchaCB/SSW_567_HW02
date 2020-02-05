8170bcaad119ab167e66bdadbe999e1e987c6a58
RuchaCB <36187469+RuchaCB@users.noreply.github.com>
2020-02-04 20:53:33 -0500

    Update TestTriangle.py

--- TestTriangle.py
+++ TestTriangle.py
@@ -23,8 +23,35 @@ class TestTriangles(unittest.TestCase):
     def testRightTriangleB(self): 
         self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
         
-    def testEquilateralTriangles(self): 
+    def testEquilateralTrianglesA(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+
+    def testEquilateralTrianglesB(self): 
+        self.assertEqual(classifyTriangle(9.4567853,9.4567853,9.4567853),'InvalidInput 3','9.4567853,9.4567853,9.4567853 should be InvalidInput 3')
+
+    def testNotATriangleTriangles(self): 
+        self.assertEqual(classifyTriangle(1,2,3),'NotATriangle','1,2,3 should be NotATriangle')
+
+    def testScaleneTrianglesA(self): 
+        self.assertEqual(classifyTriangle(5,4,3),'Scalene','5,4,3 should be scalene')
+
+    def testIsocelesTrianglesA(self): 
+        self.assertEqual(classifyTriangle(2,2,3),'Isoceles','2,2,3 should be isoceles')
+
+    def testIsocelesTrianglesB(self): 
+        self.assertEqual(classifyTriangle(3,2,2),'Isoceles','3,2,2 should be isoceles')
+
+    def testInvalidTrianglesA(self): 
+        self.assertEqual(classifyTriangle(3,2,0),'InvalidInput 2','3,2,0 should be InvalidInput 2')
+
+    def testInvalidTrianglesB(self): 
+        self.assertEqual(classifyTriangle(3,2,300),'InvalidInput 1','3,2,300 should be InvalidInput 1')
+
+    def testInvalidTrianglesC(self): 
+        self.assertEqual(classifyTriangle(-2,3,4),'InvalidInput 2','-2,3,4 should be InvalidInput 2')
+
+    def testScaleneTrianglesB(self): 
+        self.assertEqual(classifyTriangle(10,7,15),'Scalene','10,7,15 should be scalene')
 
 if __name__ == '__main__':
     print('Running unit tests')
